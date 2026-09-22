ede5a4d4b49f1712a935c0c835643196a1c14b73
huyhai1994 <nguyenhuyhai926@gmail.com>
2026-09-22 14:01:43 +0700

    test redis connection with redis test container

--- src/test/java/org/mini_lab/notificationservice/support/AbstractIntegrationTest.java
+++ src/test/java/org/mini_lab/notificationservice/support/AbstractIntegrationTest.java
@@ -2,6 +2,8 @@ package org.mini_lab.notificationservice.support;
 
 import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -11,8 +13,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Testcontainers
 public class AbstractIntegrationTest {
     @Container
-    protected static final RedisContainer REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis:6.2.6"));
+    protected static final RedisContainer REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis:6.2.6")).withExposedPorts(6379);
 
+    @DynamicPropertySource
+    private static void registerRedisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379).toString());
+    }
 
     @Test
     void test() {
